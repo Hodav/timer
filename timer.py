da5e1f4847c5ced1dbe564eac862e381b0156f7a
Vlad <baran-4ay0@yandex.ru>
2024-01-31 15:56:07 +0300

    rename main file, add timer on 1.5 hour

--- timer.py
+++ timer.py
@@ -6,28 +6,31 @@ import time
 class Timer(Frame):
     def __init__(self):
         super().__init__()
-
-        self.label = Label(background="white", font=("Arial", 32), anchor="ne")
-        self.label.pack()
-
+        self.clock = Label(background="white", font=("Arial", 24), anchor="ne")
+        self.clock.pack()
         self.timing = time.time()
         self.update_clock()
 
-    def update_clock(self):
+    def update_clock(self) -> None:
+        time_now = time.strftime("%H:%M:%S")
         timer = str(round(time.time() - self.timing) // 3600).rjust(2, "0") + ":" + str(round(time.time() - self.timing) // 60 % 60).rjust(2, "0") + ":" + str(
             round(time.time() - self.timing) % 60).rjust(2, "0")
-        self.label.configure(text=(time.strftime("%H:%M:%S") + "   " + timer))
+        time_down = 60 * 60 * 1.5
+        timer_down = str(round(time_down - (time.time() - self.timing)) // 3600).rjust(2, "0") + ":" + str(
+            round(time_down - (time.time() - self.timing)) // 60 % 60).rjust(2, "0") + ":" + str(
+            round(time_down - (time.time() - self.timing)) % 60).rjust(2, "0")
+        self.clock.configure(text=(time_now + "   " + timer + "   " + timer_down))
         self.after(1000, self.update_clock)
 
 
-def main():
+def main() -> None:
     root = Tk()
     root.title("timer")
     root.iconbitmap(default="clock.ico")
-    root.geometry("500x50+0-40")
+    root.geometry("450x40+0-40")
     root.resizable(False, False)
     root["bg"] = "white"
-    root.attributes("-topmost",True)
+    root.attributes("-topmost", True)
     app = Timer()
     root.mainloop()
 
